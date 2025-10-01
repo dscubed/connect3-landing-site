fdaed3578239f5bfa7b2e221bb09bd3530f40e09
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-10-01 19:09:36 +1000

    feat: hide university

--- app/card.tsx
+++ app/card.tsx
@@ -5,7 +5,7 @@ export function Card({ data }: { data: ClubData }) {
   return (
     <div className="relative flex items-center justify-center w-full aspect-video p-4 bg-secondary rounded-xl">
       {/* Club Name */}
-      <div className="absolute left-0 top-0 w-full p-4 text-center text-sm text-foreground/40">
+      <div className="absolute left-0 bottom-0 w-full p-4 text-center text-sm text-foreground/40">
         {data.name}
       </div>
 
@@ -13,11 +13,11 @@ export function Card({ data }: { data: ClubData }) {
       <Image src={data.image} alt={data.name} className="max-h-28 w-auto object-contain rounded-md" width={240} height={240} />
 
       {/* University */}
-      {data.uni && (
+      {/* {data.uni && (
         <div className="absolute left-0 bottom-0 w-full p-4 text-center text-sm text-foreground/40">
           {data.uni}
         </div>
-      )}
+      )} */}
     </div>
   );
 }
