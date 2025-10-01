b9aaca778c06e32effed756e5a679b2856752ffc
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-10-02 02:49:33 +1000

    feat: increase card radius

--- app/card.tsx
+++ app/card.tsx
@@ -3,7 +3,7 @@ import { ClubData } from "./data";
 
 export function Card({ data }: { data: ClubData }) {
   return (
-    <div className="relative flex items-center justify-center w-full h-64 p-4 bg-secondary rounded-xl">
+    <div className="relative flex items-center justify-center w-full h-64 p-4 bg-secondary rounded-3xl">
       {/* Club Name */}
       <div className="absolute left-0 bottom-0 w-full p-4 text-center text-sm text-foreground/40">
         <span className="line-clamp-1">{data.name}</span>
