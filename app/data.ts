1aff05f34ac7e7182380e8f621da693fa6d52206
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-10-02 14:05:18 +1000

    fix: remove existing clubs due to low number

--- app/data.ts
+++ app/data.ts
@@ -8,7 +8,7 @@ const unimelb = "University of Melbourne";
 const uwa = "University of Western Australia";
 
 export const clubs = [
-  { name: "DSCubed", image: "/clubs/dscubed.png", uni: unimelb },
-  { name: "DSC", image: "/clubs/dsc-uwa.jpg", uni: uwa },
-  { name: "RAID", image: "/clubs/raid.png", uni: unimelb },
+  // { name: "DSCubed", image: "/clubs/dscubed.png", uni: unimelb },
+  // { name: "DSC", image: "/clubs/dsc-uwa.jpg", uni: uwa },
+  // { name: "RAID", image: "/clubs/raid.png", uni: unimelb },
 ]
